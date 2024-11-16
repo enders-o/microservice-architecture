2e3aab9092262b9534c9e37e687789555efc6894
Ender Sotelo <endersotelo@endersoteloM1.local>
2024-11-16 15:34:44 -0800

    fix: analyzer indices out of sync

--- Dashboard/src/components/EndpointAnalyzer.js
+++ Dashboard/src/components/EndpointAnalyzer.js
@@ -6,11 +6,13 @@ export default function EndpointAnalyzer(props) {
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [index, setIndex] = useState(null);
 
     const getAnalyzer = () => {
         fetch(`http://ec2-44-230-66-254.us-west-2.compute.amazonaws.com:8110/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result)=>{
+                setIndex(rand_val);
 				console.log("Received Analyzer Results for " + props.endpoint)
                 setLog(result);
                 setIsLoaded(true);
@@ -32,7 +34,7 @@ export default function EndpointAnalyzer(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
         )
