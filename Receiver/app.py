de802753aca352adc0864e355f40815f5463f1a7
Ender Sotelo <endersotelo@endersoteloM1.local>
2024-12-05 13:06:24 -0800

    changed yaml files to uppercase, added encoding

--- Receiver/app.py
+++ Receiver/app.py
@@ -20,21 +20,23 @@ import os
 logger = logging.getLogger('basicLogger')
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     logger.info("In Test Environment")
-    app_conf_file = "/config/app_conf.yml"
-    log_conf_file = "/config/log_conf.yml"
+    APP_CONF_FILE = "/config/app_conf.yml"
+    LOG_CONF_FILE = "/config/log_conf.yml"
 else:
     logger.info("In Dev Environment")
-    app_conf_file = "app_conf.yml"
-    log_conf_file = "log_conf.yml"
-with open(app_conf_file, 'r') as f:
+    APP_CONF_FILE = "app_conf.yml"
+    LOG_CONF_FILE = "log_conf.yml"
+
+with open(APP_CONF_FILE, 'r', encoding='utf-8') as f:
     app_config = yaml.safe_load(f.read())
+
 # External Logging Configuration
-with open(log_conf_file, 'r') as f:
+with open(LOG_CONF_FILE, 'r', encoding='utf-8') as f:
     log_config = yaml.safe_load(f.read()) 
     logging.config.dictConfig(log_config)
 
-logger.info("App Conf File: %s" % app_conf_file)
-logger.info("Log Conf File: %s" % log_conf_file)
+logger.info("App Conf File: %s" % APP_CONF_FILE)
+logger.info("Log Conf File: %s" % LOG_CONF_FILE)
 
 
 
